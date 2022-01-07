ccb967188f759ff7bcdaed3d6b70c979b012b7bc
Alexei Garban <garban.valdeon@gmail.com>
2022-01-07 17:44:34 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "tawkto",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-react-helmet",
+  {
+    resolve: `gatsby-plugin-tawk.to`,
+    options: {
+      tawkId: "61d857b4f7cf527e84d0f3b3",
+      tawkKey: "8d1e25e8cdba06c7f7dfd9f37c8d028988eaa6e5",
+      // get this from the tawk script widget
+    },
+  },
+],
 };
